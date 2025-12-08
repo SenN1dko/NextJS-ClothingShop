22d98e965cb99fa3f7cd516eb41ea448f8168163
Arthur <arturopro346@gmail.com>
2025-12-08 20:54:09 +0500

    добавил тестовый эндпоинт

--- backend/src/api/v1/product.py
+++ backend/src/api/v1/product.py
@@ -1,3 +1,24 @@
+from schemas.product import ProductCard
+from schemas import DataResponse
+
 from fastapi import APIRouter
 
 router = APIRouter(tags=["product"])
+
+
+@router.get("/")
+async def get_product_cards():
+    return [
+        ProductCard(
+            name="Cat",
+            price=700,
+            image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+            id=0,
+        ),
+        ProductCard(
+            name="Koshka",
+            price=900,
+            image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+            id=1,
+        ),
+    ]
