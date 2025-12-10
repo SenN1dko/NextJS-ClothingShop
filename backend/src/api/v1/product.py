89320279a5a063b8d56fc80d002f604fbace1e06
Arthur <arturopro346@gmail.com>
2025-12-10 21:49:49 +0500

    добавил эндпоинт продукта по айди

--- backend/src/api/v1/product.py
+++ backend/src/api/v1/product.py
@@ -6,19 +6,54 @@ from fastapi import APIRouter
 router = APIRouter(tags=["product"])
 
 
+products = [
+    ProductCard(
+        name="Cat",
+        price=700,
+        image_url="https://blindfacts.com/wp-content/uploads/2018/01/The-Color-White.jpg",
+        id=0,
+    ),
+    ProductCard(
+        name="Koshka",
+        price=900,
+        image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+        id=1,
+    ),
+    ProductCard(
+        name="Koshka",
+        price=900,
+        image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+        id=2,
+    ),
+    ProductCard(
+        name="Koshka",
+        price=900,
+        image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+        id=3,
+    ),
+    ProductCard(
+        name="Koshka",
+        price=900,
+        image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+        id=4,
+    ),
+    ProductCard(
+        name="Koshka",
+        price=900,
+        image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
+        id=5,
+    ),
+]
+
+
 @router.get("/")
 async def get_product_cards():
-    return [
-        ProductCard(
-            name="Cat",
-            price=700,
-            image_url="https://blindfacts.com/wp-content/uploads/2018/01/The-Color-White.jpg",
-            id=0,
-        ),
-        ProductCard(
-            name="Koshka",
-            price=900,
-            image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgVfHORQFLyUf_rNove-xUmxIskDeMJ63REz_YIMQ6S0vCyQdkBvJos4igKspvCgpqnpy8h0xM--1uckzZIxDgyoHy37-MowkF-YzvVx8&s=10",
-            id=1,
-        ),
-    ]
+    return products
+
+
+@router.get("/{product_id}")
+async def get_product_by_id(product_id: int):
+    for product in products:
+        if product.id == product_id:
+            return product
+    return None  # Возвращает None, если продукт не найден
